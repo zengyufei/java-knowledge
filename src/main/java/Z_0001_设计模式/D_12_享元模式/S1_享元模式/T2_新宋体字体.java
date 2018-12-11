33712798de2cd5e9152d3db8a55072490be9ec1b
zengyufei <zengyufei@evergrande.com>
2018-12-11 09:51:08 +0800

    一天一个设计模式 享元模式

---
+++ src/main/java/Z_0001_设计模式/D_12_享元模式/S1_享元模式/T2_新宋体字体.java
@@ -0,0 +1,14 @@
+package Z_0001_设计模式.D_12_享元模式.S1_享元模式;
+
+/**
+ * 描述：具体字体类
+ * @author zengyufei
+ */
+public class T2_新宋体字体 implements T1_字体 {
+
+    @Override
+    public String 创建字体() {
+        return "新宋体";
+    }
+
+}
