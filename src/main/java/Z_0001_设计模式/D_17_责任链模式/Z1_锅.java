2a46e9af1924d96dfaddfa3f03712924b0b4192a
zengyufei <zengyufei@evergrande.com>
2018-12-17 21:43:29 +0800

    一天一个设计模式 责任链模式

---
+++ src/main/java/Z_0001_设计模式/D_17_责任链模式/Z1_锅.java
@@ -0,0 +1,11 @@
+package Z_0001_设计模式.D_17_责任链模式;
+
+/**
+ * 描述：失物认领
+ * @author zengyufei
+ */
+public abstract class Z1_锅 {
+
+    abstract void 反应();
+
+}
---
+++ src/main/java/Z_0001_设计模式/D_17_责任链模式/Z3_误删生产库的锅的开发.java
@@ -0,0 +1,13 @@
+package Z_0001_设计模式.D_17_责任链模式;
+
+/**
+ * 描述：
+ * @author zengyufei
+ */
+public class Z3_误删生产库的锅的开发 {
+
+    public static void 询问是不是你的锅(Z2_开发 开发) {
+        开发.反应();
+    }
+
+}
