28c90394a8366d23967c9ccfb86aa0a69217f623
zengyufei <zengyufei@evergrande.com>
2018-12-18 22:16:36 +0800

    一天学习一次 多线程 netty 入门 Pipline试用

---
+++ src/main/java/Z_0019_Netty入门IO通讯/S10_Netty处理链Pipline试用/Q2_服务端输出逻辑处理器2.java
@@ -0,0 +1,20 @@
+package Z_0019_Netty入门IO通讯.S10_Netty处理链Pipline试用;
+
+import Z_utils.Console;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+/**
+ * 描述：
+ * @author zengyufei
+ */
+public class Q2_服务端输出逻辑处理器2 extends ChannelOutboundHandlerAdapter {
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        Console.getThisClassName();
+        super.write(ctx, msg, promise);
+    }
+    
+}
---
+++ src/main/java/Z_0019_Netty入门IO通讯/S10_Netty处理链Pipline试用/Q3_服务端.java
@@ -0,0 +1,51 @@
+package Z_0019_Netty入门IO通讯.S10_Netty处理链Pipline试用;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 要实现的功能是：客户端连接成功之后，向服务端写一段数据 ，服务端收到数据之后打印，并向客户端回一段数据
+ */
+public class Q3_服务端 {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup 监听新连接子线程组 = new NioEventLoopGroup();
+        NioEventLoopGroup 数据读写子线程组 = new NioEventLoopGroup();
+
+        // 还有 BIO 模型：OioServerSocketChannel
+        Class<NioServerSocketChannel> NIO模型 = NioServerSocketChannel.class;
+        ChannelInitializer<NioSocketChannel> 子线程处理读写线程 = new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel nioSocketChannel) {
+                nioSocketChannel.pipeline().addLast(new Q1_服务端输入逻辑处理器1());
+                nioSocketChannel.pipeline().addLast(new Q1_服务端输入逻辑处理器2());
+                nioSocketChannel.pipeline().addLast(new Q1_服务端输入逻辑处理器3());
+
+                nioSocketChannel.pipeline().addLast(new Q2_服务端输出逻辑处理器1());
+                nioSocketChannel.pipeline().addLast(new Q2_服务端输出逻辑处理器2());
+                nioSocketChannel.pipeline().addLast(new Q2_服务端输出逻辑处理器3());
+            }
+        };
+
+        ServerBootstrap 启动器 = new ServerBootstrap();
+
+        启动器
+                .group(监听新连接子线程组, 数据读写子线程组)
+                .channel(NIO模型)
+                .childHandler(子线程处理读写线程);
+
+        启动器.bind(8000).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[8000]绑定成功!");
+            }else{
+                System.out.println("端口绑定失败！");
+            }
+        });
+
+
+    }
+
+}
